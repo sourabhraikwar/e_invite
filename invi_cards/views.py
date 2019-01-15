dc7ee8956beb86a2e8db076585d2e61de0ecdc06
sourabhraikwar <sourabhraikwar11111@gmail.com>
2019-01-15 14:35:34 +0530

    master confilict reset

--- invi_cards/views.py
+++ invi_cards/views.py
@@ -1,15 +1,11 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.shortcuts import render
-<<<<<<< HEAD
 from django.conf import settings
 from django.http import HttpResponse
 from .forms import *
 
-=======
-from django.http import HttpResponse
 import pyglet
->>>>>>> 95e19f66841b216605e776625f6bdb43784f678f
 
 # Create your views here.
 def home(request):
@@ -20,16 +16,13 @@ def home(request):
 
 	return render(request, 'home.html', context)
 
-<<<<<<< HEAD
 
 def Forms(request):
 	form = E_Form()
 	return render (request, 'home.html', {'form': form})
 
-=======
 frame = 0
 def on_draw(request):
 
     pyglet.image.get_buffer_manager().get_color_buffer().save(str(frame)+'.png')
     return render(request, 'home.html')
->>>>>>> 95e19f66841b216605e776625f6bdb43784f678f
