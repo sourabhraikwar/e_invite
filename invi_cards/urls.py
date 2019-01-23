e9fdf338b258708dc010a0244cff5f6421a75188
sourabhraikwar <sourabhraikwar11111@gmail.com>
2019-01-23 17:46:35 +0530

    files added to dev

--- invi_cards/urls.py
+++ invi_cards/urls.py
@@ -4,7 +4,6 @@ from .views import *
 urlpatterns = [
 	path('', home, name='home_page' ),
     path('E_Form/', Forms, name="E_Form"),
-	path('draw', on_draw, name='draw' ),
 	path('reset', reset, name='reset'),
 	path('signup', signup, name='signup'),
 	path('profile', profile, name='profile'),
